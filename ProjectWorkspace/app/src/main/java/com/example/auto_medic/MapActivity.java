551d10a610588e059e08a3781dd013699676b8b3
hanul <developer0218@naver.com>
2020-06-23 12:39:05 +0900

    Merge branch 'master' of https://github.com/upcake/Project_C
    # Conflicts:
    #	ProjectWorkspace/app/src/main/AndroidManifest.xml

---
+++ ProjectWorkspace/app/src/main/java/com/example/auto_medic/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.auto_medic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+    //박형규가 만든거
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+
+    }
+}
