5cc36415f8e577645fdba5aa1b166aa4c320b45e
upcake <bermwoo@gmail.com>
2020-06-24 10:53:26 +0900

    메인프래그먼트, 메인액티비티

--- ProjectWorkspace/app/src/main/java/com/example/auto_medic/DBA.sql
+++ ProjectWorkspace/app/src/main/java/com/example/auto_medic/DBA.sql
@@ -1,4 +1,4 @@
---회원 관리 테이블
+--회원 관리 테이블------------------------------------------------------------------------
 CREATE TABLE manage_member(
     member_email VARCHAR2(50),
     member_password VARCHAR2(50) NOT NULL,
@@ -21,16 +21,80 @@ VALUES ('ccccc@naver.com', '@@ccc123', '다다CCC', '010-3333-3333');
 
 SELECT * FROM manage_member;
 
---복용 기록 테이블
+
+--복용 기록 테이블----------------------------------------------------------------------------------
 CREATE TABLE medicine_record(
     record_email VARCHAR2(50),
     record_alarm_name VARCHAR2(50) NOT NULL,
     record_take_time VARCHAR2(30) NOT NULL,
-    CONSTRAINT record_email_pk PRIMARY KEY(record_email),
     CONSTRAINT record_email_fk FOREIGN KEY(record_email) REFERENCES manage_member(member_email)
 );
 
 INSERT INTO medicine_record
-VALUES ('aaaaa@naver.com', '타이레놀 2정', SYSDATE);
+VALUES ('aaaaa@naver.com', '타이레놀 2정', TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('aaaaa@naver.com', '타이레놀 2정', TO_CHAR(SYSDATE + 1, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('aaaaa@naver.com', '타이레놀 2정', TO_CHAR(SYSDATE + 2, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('bbbbb@google.com', '아스피린', TO_CHAR(SYSDATE - 15, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('bbbbb@google.com', '아스피린', TO_CHAR(SYSDATE - 14, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('bbbbb@google.com', '아스피린', TO_CHAR(SYSDATE - 10, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('ccccc@naver.com', '게보린 1', TO_CHAR(SYSDATE - 10, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('ccccc@naver.com', '게보린 1', TO_CHAR(SYSDATE - 9, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('ccccc@naver.com', '게보린 1', TO_CHAR(SYSDATE - 8, 'YYYY-MM-DD HH24:MI:SS'));
+
+INSERT INTO medicine_record
+VALUES ('ccccc@naver.com', '게보린 1', TO_CHAR(SYSDATE - 7, 'YYYY-MM-DD HH24:MI:SS'));
+
+DELETE FROM medicine_record;
+
+DROP TABLE medicine_record;
 
 SELECT * FROM medicine_record;
+
+
+--비상 연락망 테이블---------------------------------------------------------------------------
+CREATE TABLE emergency_contact(
+    emergency_email VARCHAR2(50),
+    emergency_name VARCHAR2(30) NOT NULL,
+    emergency_phonenum VARCHAR2(20) NOT NULL,
+    CONSTRAINT emergency_email_fk FOREIGN KEY(emergency_email) REFERENCES manage_member(member_email)
+);
+
+INSERT INTO emergency_contact
+VALUES ('aaaaa@naver.com', '어머니', '0101111234');
+
+INSERT INTO emergency_contact
+VALUES ('aaaaa@naver.com', '아버지', '01012341111');
+
+INSERT INTO emergency_contact
+VALUES ('bbbbb@google.com', '남편', '01022222345');
+
+INSERT INTO emergency_contact
+VALUES ('bbbbb@google.com', '친구1', '01012342222');
+
+INSERT INTO emergency_contact
+VALUES ('bbbbb@google.com', '친구2', '01023452345');
+
+INSERT INTO emergency_contact
+VALUES ('ccccc@naver.com', '아들', '01033333456');
+
+DELETE FROM emergency_contact;
+
+DROP TABLE emergency_contact;
+
+SELECT * FROM emergency_contact;
--- ProjectWorkspace/app/src/main/java/com/example/auto_medic/MainActivity.java
+++ ProjectWorkspace/app/src/main/java/com/example/auto_medic/MainActivity.java
@@ -11,10 +11,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     //프래그먼트 객체 선언
-
+    AlarmFragment alarmFragment;
     MainFragment mainFragment;
     MyPageFragment myPageFragment;
-    AlarmFragment alarmFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //프래그먼트 객체 초기화
-        mainFragment = new Fragment1();
-        myPageFragment = new Fragment2();
-        alarmFragment = new Fragment3();
+        alarmFragment = new AlarmFragment();
+        mainFragment = new MainFragment();
+        myPageFragment = new MyPageFragment();
 
-        //화면에 프래그먼트1 출력
+
+        //화면에 메인 프래그먼트 출력
         getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
 
         //바텀 네비게이션 객체 초기화
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.tab2);
 
         //바텀 네비게이션에 이벤트 추가
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,17 +43,17 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.tab1:
                         Toast.makeText(MainActivity.this, "첫 번째 탭", Toast.LENGTH_SHORT).show();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, alarmFragment).commit();
                         return true; //return true 밑으로는 작동이 되지 않는다.
 
                     case R.id.tab2:
                         Toast.makeText(MainActivity.this, "두 번째 탭", Toast.LENGTH_SHORT).show();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, myPageFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
                         return true;
 
                     case R.id.tab3:
                         Toast.makeText(MainActivity.this, "세 번째 탭", Toast.LENGTH_SHORT).show();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, alarmFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, myPageFragment).commit();
                         return true;
                 }
                 return false;
