e81c09d700cecbb363af49d7455b9c955f3e8954
upcake <bermwoo@gmail.com>
2020-07-20 14:40:32 +0900

    remove file

--- SpringWorkspace/AnSpring_App/src/main/java/com/csslect/app/arducommand/ArduCommand.java
+++
@@ -1,7 +0,0 @@
-package com.csslect.app.arducommand;
-
-import org.springframework.ui.Model;
-
-public interface ArduCommand {
-	public void execute(Model model);
-}
--- SpringWorkspace/AnSpring_App/src/main/java/com/csslect/app/command/ACommand.java
+++
@@ -1,7 +0,0 @@
-package com.csslect.app.command;
-
-import org.springframework.ui.Model;
-
-public interface ACommand {
-	public void execute(Model model);
-}
