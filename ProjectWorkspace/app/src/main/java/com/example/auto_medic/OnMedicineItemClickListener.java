278711c8ec60e2a6d002229cc77a38183d27a7ed
upcake <bermwoo@gmail.com>
2020-07-20 17:16:55 +0900

    remove all

--- ProjectWorkspace/app/src/main/java/com/example/auto_medic/OnMedicineItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.auto_medic;
-
-import android.view.View;
-
-public interface OnMedicineItemClickListener {
-    public void onItemClick(MedicineListAdapter.ViewHolder holderm, View view, int position);
-}
